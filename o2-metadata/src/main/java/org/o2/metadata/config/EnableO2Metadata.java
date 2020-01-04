3ffde348f636080ed06beb3e26581d911d2d4b23
鲍传琦 <mark.bao@hand-china.com>
2020-01-04 16:26:55 +0800

    [FIX]微服务基础改造

--- o2-metadata/src/main/java/org/o2/metadata/config/EnableO2Metadata.java
+++ o2-metadata/src/main/java/org/o2/metadata/config/EnableO2Metadata.java
@@ -11,5 +11,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Import(MetadataAutoConfiguration.class)
-public @interface EnableMetadata {
+public @interface EnableO2Metadata {
 }
