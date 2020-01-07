fe6f332e8555e28578f36404396174ab19f53bbe
鲍传琦 <mark.bao@hand-china.com>
2020-01-07 19:49:56 +0800

    [FIX]微服务基础改造

--- o2-metadata/src/main/java/org/o2/metadata/config/MetadataAutoConfiguration.java
+++ o2-metadata/src/main/java/org/o2/metadata/config/MetadataAutoConfiguration.java
@@ -3,7 +3,6 @@ package org.o2.metadata.config;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.o2.context.metadata.api.IPosContext;
 import org.o2.context.metadata.api.ISysParameterContext;
-import org.o2.core.resource.annotation.EnableO2Server;
 import org.o2.data.redis.client.RedisCacheClient;
 import org.o2.metadata.api.rpc.PosContextImpl;
 import org.o2.metadata.api.rpc.SysParameterContextImpl;
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
  * @author mark.bao@hand-china.com 2020/1/2
  */
 @DubboComponentScan("org.o2.metadata.api.rpc")
-@EnableO2Server
 @Configuration
 public class MetadataAutoConfiguration {
 
