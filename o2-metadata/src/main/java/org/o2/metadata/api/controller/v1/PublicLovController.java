2be56f0772588158a010bcabe0acb379ba31bdbf
cWww <koudaidemon@163.com>
2020-01-10 17:20:57 +0800

    [FIX] 添加值集公共开放接口,添加多语言方式

--- o2-metadata/src/main/java/org/o2/metadata/api/controller/v1/PublicLovController.java
+++ o2-metadata/src/main/java/org/o2/metadata/api/controller/v1/PublicLovController.java
@@ -1,8 +1,10 @@
 package org.o2.metadata.api.controller.v1;
 
+import io.choerodon.core.oauth.DetailsHelper;
 import io.choerodon.swagger.annotation.Permission;
 import io.swagger.annotations.*;
 import org.hzero.boot.platform.lov.dto.LovValueDTO;
+import org.hzero.core.base.BaseConstants;
 import org.o2.metadata.core.infra.service.CustomLovService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +22,7 @@ import java.util.Map;
 @Api(
         tags = {"Public Lov"}
 )
-@RestController("customLovController.v1")
+@RestController("publicLovController.v1")
 @RequestMapping({"/v1/{organizationId}/lov"})
 public class PublicLovController {
 
@@ -34,8 +36,16 @@ public class PublicLovController {
     @Permission(permissionPublic = true)
     @GetMapping({"/search-by-code"})
     public ResponseEntity<List<LovValueDTO>> searchProduct(@RequestParam @ApiParam(value = "值集编码", required = true) String lovCode,
+                                                           @RequestParam(required = false, defaultValue = "zh_CN") @ApiParam(value = "语言", defaultValue = "zh_CN")final String lang,
                                                            @PathVariable @ApiParam(value = "租户ID", required = true) Long organizationId) {
-        return customLovService.queryLovInfo(lovCode, organizationId);
+        if (null == DetailsHelper.getUserDetails()) {
+            DetailsHelper.getAnonymousDetails().setLanguage(lang);
+            final ResponseEntity<List<LovValueDTO>> responseEntity = customLovService.queryLovInfo(lovCode, organizationId);
+            DetailsHelper.getAnonymousDetails().setLanguage(BaseConstants.DEFAULT_LOCALE_STR);
+            return responseEntity;
+        } else {
+            return customLovService.queryLovInfo(lovCode, organizationId);
+        }
     }
 
     @ApiOperation("集值 - 批量查询")
@@ -49,7 +59,14 @@ public class PublicLovController {
             required = true
     )})
     public ResponseEntity<Map<String, List<LovValueDTO>>> batchSearchProduct(@RequestParam Map<String, String> queryMap,
-                                                                       @PathVariable @ApiParam(value = "租户ID", required = true) Long organizationId) {
+                                                                             @RequestParam(required = false, defaultValue = "zh_CN") @ApiParam(value = "语言", defaultValue = "zh_CN")final String lang,
+                                                                             @PathVariable @ApiParam(value = "租户ID", required = true) Long organizationId) {
+        if (null == DetailsHelper.getUserDetails()) {
+            DetailsHelper.getAnonymousDetails().setLanguage(lang);
+            final ResponseEntity<Map<String, List<LovValueDTO>>> responseEntity = customLovService.batchQueryLovInfo(queryMap, organizationId);
+            DetailsHelper.getAnonymousDetails().setLanguage(BaseConstants.DEFAULT_LOCALE_STR);
+            return responseEntity;
+        }
         return customLovService.batchQueryLovInfo(queryMap, organizationId);
     }
 }
