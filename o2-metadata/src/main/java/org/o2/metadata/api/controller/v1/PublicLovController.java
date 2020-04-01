f49a85f57bfd54066066c9fefba5c4e9d4365858
mark.bao <mark.bao@hand-china.com>
2020-04-01 10:39:15 +0800

    [FIX]格式调整

--- o2-metadata/src/main/java/org/o2/metadata/api/controller/v1/PublicLovController.java
+++ o2-metadata/src/main/java/org/o2/metadata/api/controller/v1/PublicLovController.java
@@ -2,7 +2,9 @@ package org.o2.metadata.api.controller.v1;
 
 import io.choerodon.core.oauth.DetailsHelper;
 import io.choerodon.swagger.annotation.Permission;
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.hzero.boot.platform.lov.dto.LovValueDTO;
 import org.hzero.core.base.BaseConstants;
 import org.o2.metadata.core.infra.service.CustomLovService;
@@ -25,19 +27,18 @@ import java.util.Map;
 @RestController("publicLovController.v1")
 @RequestMapping({"/v1/{organizationId}/lov"})
 public class PublicLovController {
+    private final CustomLovService customLovService;
 
-    private CustomLovService customLovService;
-
-    public PublicLovController(CustomLovService customLovService){
+    public PublicLovController(final CustomLovService customLovService) {
         this.customLovService = customLovService;
     }
 
     @ApiOperation("集值 - 查询")
     @Permission(permissionPublic = true)
     @GetMapping({"/search-by-code"})
-    public ResponseEntity<List<LovValueDTO>> searchLov(@RequestParam @ApiParam(value = "值集编码", required = true) String lovCode,
-                                                           @RequestParam(required = false, defaultValue = "zh_CN") @ApiParam(value = "语言", defaultValue = "zh_CN")final String lang,
-                                                           @PathVariable @ApiParam(value = "租户ID", required = true) Long organizationId) {
+    public ResponseEntity<List<LovValueDTO>> searchLov(@RequestParam @ApiParam(value = "值集编码", required = true) final String lovCode,
+                                                       @RequestParam(required = false, defaultValue = "zh_CN") @ApiParam(value = "语言", defaultValue = "zh_CN") final String lang,
+                                                       @PathVariable @ApiParam(value = "租户ID", required = true) final Long organizationId) {
         try {
             DetailsHelper.getAnonymousDetails().setLanguage(lang);
             return customLovService.queryLovInfo(lovCode, organizationId);
@@ -49,9 +50,9 @@ public class PublicLovController {
     @ApiOperation("集值 - 批量查询")
     @Permission(permissionPublic = true)
     @GetMapping({"/batch/search-by-code"})
-    public ResponseEntity<Map<String, List<LovValueDTO>>> batchSearchLov(@RequestParam Map<String, String> queryMap,
-                                                                             @RequestParam(required = false, defaultValue = "zh_CN") @ApiParam(value = "语言", defaultValue = "zh_CN")final String lang,
-                                                                             @PathVariable @ApiParam(value = "租户ID", required = true) Long organizationId) {
+    public ResponseEntity<Map<String, List<LovValueDTO>>> batchSearchLov(@RequestParam final Map<String, String> queryMap,
+                                                                         @RequestParam(required = false, defaultValue = "zh_CN") @ApiParam(value = "语言", defaultValue = "zh_CN") final String lang,
+                                                                         @PathVariable @ApiParam(value = "租户ID", required = true) final Long organizationId) {
         try {
             DetailsHelper.getAnonymousDetails().setLanguage(lang);
             return customLovService.batchQueryLovInfo(queryMap, organizationId);
