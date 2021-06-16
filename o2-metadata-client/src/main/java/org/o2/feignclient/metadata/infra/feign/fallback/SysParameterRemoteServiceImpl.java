723b500e9e26f2a6c669a31b7346b3bc0882b826
yipeng.zhu <yipeng.zhu@hand-china.com>
2021-06-16 11:26:57 +0800

    feat(dev): 元数据模块微服务改造

---
+++ o2-metadata-client/src/main/java/org/o2/feignclient/metadata/infra/feign/fallback/SysParameterRemoteServiceImpl.java
@@ -0,0 +1,23 @@
+package org.o2.feignclient.metadata.infra.feign.fallback;
+
+import org.o2.feignclient.metadata.infra.feign.SysParameterRemoteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lei.tang02@hand-china.com 2020/8/27
+ */
+@Component
+public class SysParameterRemoteServiceImpl implements SysParameterRemoteService {
+    private static final Logger logger = LoggerFactory.getLogger(SysParameterRemoteServiceImpl.class);
+
+
+    @Override
+    public ResponseEntity<String> listSystemParameter(Long organizationId, String paramCode) {
+        logger.error("Error saveSysParameter, params[tenantId = {}, code = {}]", organizationId, paramCode);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+}
