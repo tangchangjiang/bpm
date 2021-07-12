2ccefe95fc30d65ca356eadc2fe39a2333b5e1c4
yipeng.zhu <yipeng.zhu@hand-china.com>
2021-07-12 16:25:37 +0800

    fix(dev):系统参数数据库和redis一致性

--- o2-metadata-management/src/main/java/org/o2/metadata/console/infra/redis/impl/SystemParameterRedisImpl.java
+++ o2-metadata-management/src/main/java/org/o2/metadata/console/infra/redis/impl/SystemParameterRedisImpl.java
@@ -156,7 +156,6 @@ public class SystemParameterRedisImpl implements SystemParameterRedis {
     }
     @Override
     public void updateToRedis(SystemParameter systemParameter, Long tenantId) {
-        int u= 3/0;
         final String paramTypeCode = systemParameter.getParamTypeCode();
         final String paramCode = systemParameter.getParamCode();
         //参数redis更新
