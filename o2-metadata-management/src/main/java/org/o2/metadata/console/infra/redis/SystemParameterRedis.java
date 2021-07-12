097fb77a9e3d630405c6c9fe129a8d18f8c63f23
yipeng.zhu <yipeng.zhu@hand-china.com>
2021-07-12 15:57:13 +0800

    fix(dev):系统参数数据库和redis一致性

--- o2-metadata-management/src/main/java/org/o2/metadata/console/infra/redis/SystemParameterRedis.java
+++ o2-metadata-management/src/main/java/org/o2/metadata/console/infra/redis/SystemParameterRedis.java
@@ -15,12 +15,13 @@ public interface SystemParameterRedis {
      * redis 查询系统参数
      * @param paramCode 编码
      * @param tenantId 租户ID
-     * @return
+     * @return 系统参数
      */
     SystemParameter getSystemParameter(String paramCode, Long tenantId);
     /**
      * 批量 redis 查询系统参数
      * @param paramCodeList 编码集合
+     * @param tenantId 租户ID
      * @return  list
      */
     List<SystemParameter> listSystemParameters(List<String> paramCodeList, Long tenantId);
@@ -40,7 +41,6 @@ public interface SystemParameterRedis {
      * @param tenantId 租户ID
      **/
     void updateToRedis(SystemParameter systemParameter,Long tenantId);
-
 
     /**
      *
