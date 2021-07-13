61dec5bb22e1e41133967f12fffc35ff4b7820ce
yipeng.zhu <yipeng.zhu@hand-china.com>
2021-07-13 13:51:52 +0800

    fix(dev):领域层的改造-查询仓库

--- o2-metadata-management-client/src/main/java/org/o2/feignclient/metadata/config/MetadataFeignClientConfiguration.java
+++ o2-metadata-management-client/src/main/java/org/o2/feignclient/metadata/config/MetadataFeignClientConfiguration.java
@@ -2,7 +2,9 @@ package org.o2.feignclient.metadata.config;
 
 import org.o2.feignclient.O2MetadataManagementClient;
 import org.o2.feignclient.metadata.infra.feign.SysParameterRemoteService;
+import org.o2.feignclient.metadata.infra.feign.WarehouseRemoteService;
 import org.o2.feignclient.metadata.infra.feign.fallback.SysParameterRemoteServiceImpl;
+import org.o2.feignclient.metadata.infra.feign.fallback.WarehouseRemoteServiceImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +13,7 @@ import org.springframework.context.annotation.Bean;
  * @author lei.tang02@hand-china.com 2020/8/27
  */
 @EnableFeignClients(
-        basePackageClasses = {SysParameterRemoteService.class}
+        basePackageClasses = {SysParameterRemoteService.class, WarehouseRemoteService.class}
 )
 public class MetadataFeignClientConfiguration {
     @Bean
@@ -20,11 +22,16 @@ public class MetadataFeignClientConfiguration {
         return new SysParameterRemoteServiceImpl();
     }
 
-
+    @Bean
+    @ConditionalOnMissingBean
+    public WarehouseRemoteServiceImpl warehouseRemoteServiceFallback() {
+        return new WarehouseRemoteServiceImpl();
+    }
 
     @Bean
     @ConditionalOnMissingBean
-    O2MetadataManagementClient o2MetadataManagementClient(SysParameterRemoteService sysParameterRemoteService) {
+    O2MetadataManagementClient o2MetadataManagementClient(SysParameterRemoteService sysParameterRemoteService,
+                                                          WarehouseRemoteService warehouseRemoteService) {
         return new O2MetadataManagementClient(sysParameterRemoteService, warehouseRemoteService);
     }
 }
