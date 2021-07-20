5979d48d7f6f62ec7283d029e2f9fb26c7685801
yipeng.zhu <yipeng.zhu@hand-china.com>
2021-07-20 18:39:50 +0800

    fix(dev):重写equals和hashCode

--- o2-metadata-client/src/main/java/org/o2/feignclient/metadata/domain/vo/FreightInfoVO.java
+++ o2-metadata-client/src/main/java/org/o2/feignclient/metadata/domain/vo/FreightInfoVO.java
@@ -2,6 +2,8 @@ package org.o2.feignclient.metadata.domain.vo;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 /**
  *
  * 运费模版信息
@@ -18,4 +20,19 @@ public class FreightInfoVO {
     private  FreightTemplateVO headTemplate;
 
     private  FreightTemplateDetailVO regionTemplate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {return true;}
+        if (o == null || getClass() != o.getClass()) {return false;}
+        FreightInfoVO that = (FreightInfoVO) o;
+        return freightTemplateCode.equals(that.freightTemplateCode) &&
+                headTemplate.equals(that.headTemplate) &&
+                regionTemplate.equals(that.regionTemplate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(freightTemplateCode, headTemplate, regionTemplate);
+    }
 }
