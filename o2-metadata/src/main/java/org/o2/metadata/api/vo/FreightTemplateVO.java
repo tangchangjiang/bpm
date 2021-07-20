0be188ff072800e03774f408f2654371b24b52f3
yipeng.zhu <yipeng.zhu@hand-china.com>
2021-07-20 11:18:46 +0800

    fix(dev):运费模块-C端获取运费模版方法返参修改

---
+++ o2-metadata/src/main/java/org/o2/metadata/api/vo/FreightTemplateVO.java
@@ -0,0 +1,38 @@
+package org.o2.metadata.api.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+
+/**
+ * 运费模板
+ *
+ * @author peng.xu@hand-china.com 2019/5/15
+ */
+@ApiModel("运费模板")
+@Data
+public class FreightTemplateVO {
+
+    @ApiModelProperty("表ID，主键")
+    private Long templateId;
+    @ApiModelProperty(value = "运费模板编码")
+    private String templateCode;
+    @ApiModelProperty(value = "运费模板名称")
+    private String templateName;
+    @ApiModelProperty(value = "是否包邮")
+    private Integer deliveryFreeFlag;
+    @ApiModelProperty(value = "计价方式，值集O2MD.VALUATION_TYPE")
+    private String valuationType;
+    @ApiModelProperty(value = "计价单位，值集O2MD.UOM")
+    private String valuationUom;
+    @ApiModelProperty(value = "默认运费模板标记，新建的时候默认为0")
+    private Integer dafaultFlag;
+    @ApiModelProperty(value = "租户ID")
+    private Long tenantId;
+    @ApiModelProperty(value = "计价方式描述")
+    private String valuationTypeMeaning;
+    @ApiModelProperty(value = "计价单位描述")
+    private String valuationUomMeaning;
+    
+}
--- o2-metadata/src/main/java/org/o2/metadata/infra/redis/impl/FreightRedisImpl.java
+++ o2-metadata/src/main/java/org/o2/metadata/infra/redis/impl/FreightRedisImpl.java
@@ -1,9 +1,12 @@
 package org.o2.metadata.infra.redis.impl;
 
 import org.apache.commons.lang3.StringUtils;
+import org.o2.core.helper.FastJsonHelper;
 import org.o2.data.redis.client.RedisCacheClient;
 import org.o2.metadata.infra.constants.FreightConstants;
 import org.o2.metadata.infra.entity.FreightInfo;
+import org.o2.metadata.infra.entity.FreightTemplate;
+import org.o2.metadata.infra.entity.FreightTemplateDetail;
 import org.o2.metadata.infra.redis.FreightRedis;
 import org.springframework.stereotype.Component;
 
@@ -29,9 +32,12 @@ public class FreightRedisImpl implements FreightRedis {
         FreightInfo freightInfo = new FreightInfo();
         String freightDetailKey = FreightConstants.RedisKey.getFreightDetailKey(tenantId, templateCode);
         List<String> freightTemplates = redisCacheClient.<String, String>opsForHash().multiGet(freightDetailKey, Arrays.asList(FreightConstants.RedisKey.FREIGHT_HEAD_KEY, FreightConstants.RedisKey.FREIGHT_DEFAULT_KEY, regionCode));
-        freightInfo.setHeadTemplate(freightTemplates.get(0));
+        String headTemplate =  freightTemplates.get(0);
+        freightInfo.setHeadTemplate(StringUtils.isEmpty(headTemplate) ? null : FastJsonHelper.stringToObject(headTemplate, FreightTemplate.class));
+
+
         String cityTemplate = StringUtils.isEmpty(freightTemplates.get(2)) ? freightTemplates.get(1) : freightTemplates.get(2);
-        freightInfo.setRegionTemplate(cityTemplate);
+        freightInfo.setRegionTemplate(StringUtils.isEmpty(cityTemplate)?null :FastJsonHelper.stringToObject(cityTemplate, FreightTemplateDetail.class));
         return freightInfo;
     }
 }
