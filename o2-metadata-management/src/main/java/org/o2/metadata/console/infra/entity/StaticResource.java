1620430024155e87f1c3ccaf3519fd22e826f283
zhanpeng.jiang <zhanpeng.jiang@hand-china.com>
2021-08-03 17:17:16 +0800

    feat(静态资源表): 添加lang字段，编码去掉lang

--- o2-metadata-management/src/main/java/org/o2/metadata/console/infra/entity/StaticResource.java
+++ o2-metadata-management/src/main/java/org/o2/metadata/console/infra/entity/StaticResource.java
@@ -1,17 +1,19 @@
 package org.o2.metadata.console.infra.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.hzero.mybatis.annotation.Unique;
+
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import javax.validation.constraints.NotBlank;
-import io.choerodon.mybatis.domain.AuditDomain;
-import org.hzero.mybatis.annotation.Unique;
 import io.choerodon.mybatis.annotation.ModifyAudit;
 import io.choerodon.mybatis.annotation.VersionAudit;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.choerodon.mybatis.domain.AuditDomain;
 
 /**
  * 静态资源文件表
@@ -29,6 +31,7 @@ public class StaticResource extends AuditDomain {
     public static final String FIELD_SOURCE_MODULE_CODE = "sourceModuleCode";
     public static final String FIELD_RESOURCE_URL = "resourceUrl";
     public static final String FIELD_DESCRIPTION = "description";
+    public static final String FIELD_LANG = "lang";
     public static final String FIELD_TENANT_ID = "tenantId";
     public static final String O2MD_STATIC_RESOURCE_U1 = "o2md_static_resource_u1";
 
@@ -57,12 +60,16 @@ public class StaticResource extends AuditDomain {
     private String resourceUrl;
     @ApiModelProperty(value = "静态资源描述")
     private String description;
+
+    @ApiModelProperty(value = "语言")
+    private String lang;
+
     @ApiModelProperty(value = "租户Id", required = true)
     @NotNull
     @Unique(O2MD_STATIC_RESOURCE_U1)
     private Long tenantId;
 
-	//
+    //
     // 非数据库字段
     // ------------------------------------------------------------------------------
     //
@@ -72,63 +79,75 @@ public class StaticResource extends AuditDomain {
     /**
      * @return 主键
      */
-	public Long getResourceId() {
-		return resourceId;
-	}
+    public Long getResourceId() {
+        return resourceId;
+    }
 
-	public void setResourceId(Long resourceId) {
-		this.resourceId = resourceId;
-	}
+    public void setResourceId(Long resourceId) {
+        this.resourceId = resourceId;
+    }
+
     /**
      * @return 静态资源编码[模块名_资源内容名]
      */
-	public String getResourceCode() {
-		return resourceCode;
-	}
+    public String getResourceCode() {
+        return resourceCode;
+    }
 
-	public void setResourceCode(String resourceCode) {
-		this.resourceCode = resourceCode;
-	}
+    public void setResourceCode(String resourceCode) {
+        this.resourceCode = resourceCode;
+    }
+
     /**
      * @return 来源模块编码
      */
-	public String getSourceModuleCode() {
-		return sourceModuleCode;
-	}
+    public String getSourceModuleCode() {
+        return sourceModuleCode;
+    }
 
-	public void setSourceModuleCode(String sourceModuleCode) {
-		this.sourceModuleCode = sourceModuleCode;
-	}
+    public void setSourceModuleCode(String sourceModuleCode) {
+        this.sourceModuleCode = sourceModuleCode;
+    }
+
     /**
      * @return 静态资源相对路径
      */
-	public String getResourceUrl() {
-		return resourceUrl;
-	}
+    public String getResourceUrl() {
+        return resourceUrl;
+    }
 
-	public void setResourceUrl(String resourceUrl) {
-		this.resourceUrl = resourceUrl;
-	}
+    public void setResourceUrl(String resourceUrl) {
+        this.resourceUrl = resourceUrl;
+    }
+
     /**
      * @return 静态资源描述
      */
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     /**
      * @return 租户Id
      */
-	public Long getTenantId() {
-		return tenantId;
-	}
+    public Long getTenantId() {
+        return tenantId;
+    }
 
-	public void setTenantId(Long tenantId) {
-		this.tenantId = tenantId;
-	}
+    public void setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+    }
 
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
 }
 
