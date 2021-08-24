f79fef1024af3959f56304713cf8dec6ea340c0c
zhanpeng.jiang <zhanpeng.jiang@hand-china.com>
2021-08-24 18:35:30 +0800

    feat(静态资源表): 解决冲突

--- o2-metadata-management/src/main/java/org/o2/metadata/console/infra/constant/MetadataConstants.java
+++ o2-metadata-management/src/main/java/org/o2/metadata/console/infra/constant/MetadataConstants.java
@@ -281,11 +281,6 @@ public interface MetadataConstants {
          */
         String O2MD_REGION_DESCRIPTION = "省市区数据文件";
 
-        /**
-         * 元数据 - 商城端公共的Lov值集
-         */
-        String O2MD_PUB_LOV = "O2MD_PUB_LOV";
-
 
         /**
          * 元数据 - 值集数据文件
