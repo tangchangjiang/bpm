a5b24ebf1cfa28cfc2dc4144fc3147a572e7304f
yipeng.zhu <yipeng.zhu@hand-china.com>
2021-09-22 11:01:38 +0800

    fix(dev):单位值集client 拆分

--- o2-metadata-management/src/main/java/org/o2/metadata/console/infra/lovadapter/repository/BaseLovQueryRepository.java
+++ o2-metadata-management/src/main/java/org/o2/metadata/console/infra/lovadapter/repository/BaseLovQueryRepository.java
@@ -8,19 +8,12 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 基础LOVService,具有业务属性
+ * 基础LOVService,具有业务属性 基本单位
  *
  * @author wei.cai@hand-china.com 2021/8/24
  */
 public interface BaseLovQueryRepository {
 
-    /**
-     * 通过编码查询货币
-     * @param tenantId 租户ID
-     * @param currencyCode 货币编码
-     * @return 查找货币信息
-     */
-    CurrencyBO findCurrencyByCode(Long tenantId, String currencyCode);
 
     /**
      * 通过编码查询货币(批量)
@@ -30,13 +23,6 @@ public interface BaseLovQueryRepository {
      */
     Map<String, CurrencyBO> findCurrencyByCodes(Long tenantId, List<String> currencyCodes);
 
-    /**
-     * 通过编码查询单位
-     * @param tenantId 租户ID
-     * @param uomCode 单位编码
-     * @return 单位信息
-     */
-    UomBO findUomByCode(Long tenantId, String uomCode);
 
     /**
      * 通过编码查询单位(批量)
@@ -46,13 +32,6 @@ public interface BaseLovQueryRepository {
      */
     Map<String, UomBO> findUomByCodes(Long tenantId, List<String> uomCodes);
 
-    /**
-     * 通过编码查询单位类型
-     * @param tenantId 租户ID
-     * @param uomTypeCode 单位类型编码
-     * @return 单位类型MAP
-     */
-    UomTypeBO findUomTypeByCode(Long tenantId, String uomTypeCode);
 
     /**
      * 通过编码查询单位类型(批量)
