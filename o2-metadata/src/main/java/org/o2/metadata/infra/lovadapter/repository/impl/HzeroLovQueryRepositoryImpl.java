3b91c90078c8d82904ceaaef861c94842011ea47
yipeng.zhu <yipeng.zhu@hand-china.com>
2021-09-24 18:02:51 +0800

    fix(dev):独立值集缓存

--- o2-metadata/src/main/java/org/o2/metadata/infra/lovadapter/repository/impl/HzeroLovQueryRepositoryImpl.java
+++ o2-metadata/src/main/java/org/o2/metadata/infra/lovadapter/repository/impl/HzeroLovQueryRepositoryImpl.java
@@ -4,15 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.hzero.boot.platform.lov.adapter.LovAdapter;
-import org.o2.metadata.infra.constants.O2LovConstants;
+import org.hzero.boot.platform.lov.dto.LovValueDTO;
 import org.o2.metadata.infra.lovadapter.repository.HzeroLovQueryRepository;
-import org.springframework.aop.framework.AopContext;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
 /**
  *
  * lov
@@ -31,34 +28,7 @@ public class HzeroLovQueryRepositoryImpl implements HzeroLovQueryRepository {
 
 
     @Override
-    public String queryLovValueMeaning(Long tenantId,
-                                       String lovCode,
-                                       String lovValue) {
-        StringBuilder cacheKey = new StringBuilder(tenantId.toString()).append("_").append(lovCode).append("_").append(lovValue);
-        HzeroLovQueryRepositoryImpl currentProxy = (HzeroLovQueryRepositoryImpl) AopContext.currentProxy();
-        List<Map<String, Object>> lovList = currentProxy.queryLovValueMeaning(tenantId, lovCode, null,null,null, cacheKey.toString());
-        if (CollectionUtils.isEmpty(lovList) || MapUtils.isEmpty(lovList.get(0))) {
-            return "";
-        } else {
-            String lovMeaning = "";
-            for (Map<String, Object> lov : lovList) {
-                if (lovValue.equals(lov.get(O2LovConstants.LovProperties.lovValue))) {
-                    lovMeaning = (String) lov.get(O2LovConstants.LovProperties.lovMeaning);
-                    break;
-                }
-            }
-            return lovMeaning;
-        }
-    }
-
-
-    @Cacheable(value = "O2_LOV", key = "#cacheKey")
-    public List<Map<String, Object>> queryLovValueMeaning(Long tenantId,
-                                                          String lovCode,
-                                                          Integer page,
-                                                          Integer size,
-                                                          Map<String, String> queryLovValueMap,
-                                                          String cacheKey) {
-        return lovAdapter.queryLovData(lovCode, tenantId, null, page, size, queryLovValueMap);
+    public List<LovValueDTO> queryLovValue(Long tenantId, String lovCode) {
+        return lovAdapter.queryLovValue(lovCode, tenantId);
     }
 }
---
+++ o2-metadata/src/main/java/org/o2/metadata/infra/lovadapter/repository/impl/IdpLovQueryRepositoryImpl.java
@@ -0,0 +1,44 @@
+package org.o2.metadata.infra.lovadapter.repository.impl;
+
+import org.hzero.boot.platform.lov.dto.LovValueDTO;
+import org.hzero.core.base.AopProxy;
+import org.o2.metadata.infra.lovadapter.repository.HzeroLovQueryRepository;
+import org.o2.metadata.infra.lovadapter.repository.IdpLovQueryRepository;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ *
+ * 独立值集查询
+ *
+ * @author yipeng.zhu@hand-china.com 2021-09-23
+ **/
+@Repository
+public class IdpLovQueryRepositoryImpl implements IdpLovQueryRepository, AopProxy<IdpLovQueryRepositoryImpl> {
+    private HzeroLovQueryRepository hzeroLovQueryRepository;
+
+    public IdpLovQueryRepositoryImpl(HzeroLovQueryRepository hzeroLovQueryRepository) {
+        this.hzeroLovQueryRepository = hzeroLovQueryRepository;
+    }
+
+    @Override
+    @Cacheable(value = "O2_LOV", key = "'idp'+'_'+#tenantId+'_'+#lovCode")
+    public List<LovValueDTO> queryLovValue(Long tenantId, String lovCode) {
+        return hzeroLovQueryRepository.queryLovValue(tenantId,lovCode);
+    }
+
+    @Override
+    public String queryLovValueMeaning(Long tenantId, String lovCode, String lovValue) {
+        List<LovValueDTO> list = self().queryLovValue(tenantId,lovCode);
+        if (!list.isEmpty()) {
+            for (LovValueDTO dto : list) {
+                if (dto.getValue().equals(lovValue)){
+                    return dto.getMeaning();
+                }
+            }
+        }
+        return "";
+    }
+}
