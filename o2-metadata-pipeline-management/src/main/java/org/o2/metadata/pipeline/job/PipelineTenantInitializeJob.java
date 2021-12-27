b448d2104c31f644e75f83bde2aabaa411a4292b
zhanpeng.jiang <zhanpeng.jiang@hand-china.com>
2021-12-27 16:36:29 +0800

    fix(租户初始化job): sourceTenantId改为sourceTenant

--- o2-metadata-pipeline-management/src/main/java/org/o2/metadata/pipeline/job/PipelineTenantInitializeJob.java
+++ o2-metadata-pipeline-management/src/main/java/org/o2/metadata/pipeline/job/PipelineTenantInitializeJob.java
@@ -26,7 +26,7 @@ public class PipelineTenantInitializeJob implements IJobHandler {
 
     private static final String TARGET_TENANT_IDS = "targetTenants";
 
-    private static final String SOURCE_TENANT_ID = "sourceTenantId";
+    private static final String SOURCE_TENANT_ID = "sourceTenant";
 
     private final PipelineTenantInitService pipelineTenantInitService;
 
