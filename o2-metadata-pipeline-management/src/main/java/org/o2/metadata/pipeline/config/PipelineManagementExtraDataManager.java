91ed4b8c5382330217207e7bcf2772789c0d3488
赵德明 <deming.zhao@hand-china.com>
2021-12-31 15:16:20 +0800

    fix(metadata): 元数据合并后feign调用问题
    元数据合并后feign调用问题

--- o2-metadata-pipeline-management/src/main/java/org/o2/metadata/pipeline/config/PipelineManagementExtraDataManager.java
+++ o2-metadata-pipeline-management/src/main/java/org/o2/metadata/pipeline/config/PipelineManagementExtraDataManager.java
@@ -21,8 +21,8 @@ public class PipelineManagementExtraDataManager implements ExtraDataManager {
         ChoerodonRouteData choerodonRouteData = new ChoerodonRouteData();
         choerodonRouteData.setName(this.environment.getProperty("o2.service.current.name", O2Service.MetadataManagement.CODE));
         choerodonRouteData.setPath(this.environment.getProperty("o2.service.current.path", O2Service.MetadataManagement.PATH));
-        choerodonRouteData.setServiceId(this.environment.getProperty("o2.service.current.service-name", O2Service.MetadataManagement.NAME));
-        choerodonRouteData.setPackages("org.o2.metadata.pipeline");
+        choerodonRouteData.setServiceId(this.environment.getProperty("o2.service.current.service-name", O2Service.MetadataManagement.ID));
+        choerodonRouteData.setPackages("org.o2.metadata.pipeline.api");
         extraData.put(ExtraData.ZUUL_ROUTE_DATA, choerodonRouteData);
         return extraData;
     }
