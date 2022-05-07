3c5b6e157bbdaf848408e5d335b4d660b68a9d4c
yipeng.zhu <yipeng.zhu@hand-china.com>
2022-05-07 15:00:27 +0800

    fix(master):优化批量查询网店关联仓库redis信息

--- o2-metadata-management/src/main/java/org/o2/metadata/console/infra/redis/impl/OnlineShopRelWarehouseRedisImpl.java
+++ o2-metadata-management/src/main/java/org/o2/metadata/console/infra/redis/impl/OnlineShopRelWarehouseRedisImpl.java
@@ -1,9 +1,17 @@
 package org.o2.metadata.console.infra.redis.impl;
 
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.o2.core.helper.JsonHelper;
 import org.o2.data.redis.client.RedisCacheClient;
+import org.o2.metadata.console.api.co.OnlineShopRelWarehouseCO;
 import org.o2.metadata.console.infra.constant.OnlineShopConstants;
 import org.o2.metadata.console.infra.entity.OnlineShopRelWarehouse;
 import org.o2.metadata.console.infra.redis.OnlineShopRelWarehouseRedis;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -16,6 +24,7 @@ import java.util.Map;
  *
  * @author yipeng.zhu@hand-china.com 2021-07-13
  **/
+@Slf4j
 @Component
 public class OnlineShopRelWarehouseRedisImpl implements OnlineShopRelWarehouseRedis {
     private final RedisCacheClient redisCacheClient;
@@ -41,5 +50,31 @@ public class OnlineShopRelWarehouseRedisImpl implements OnlineShopRelWarehouseRe
             list.add(onlineShopRelWarehouse);
         }
         return list;
+    }
+
+    @Override
+    public List<OnlineShopRelWarehouseCO> listOnlineShopRelWarehouses(List<String> onlineShopCodes, Long tenantId) {
+        List<String> keys = new ArrayList<>(onlineShopCodes.size());
+        onlineShopCodes.forEach(code->{
+            String hashKey = String.format(OnlineShopConstants.Redis.KEY_ONLINE_SHOP_REL_WAREHOUSE, tenantId, code);
+            keys.add(hashKey);
+        });
+        List<Object> objects = null;
+        try {
+            objects = redisCacheClient.executePipelined(new RedisCallback<OnlineShopRelWarehouseCO>() {
+                @Override
+                public OnlineShopRelWarehouseCO doInRedis(@NonNull RedisConnection redisConnection) throws DataAccessException {
+                    StringRedisConnection stringConnection = (StringRedisConnection) redisConnection;
+                    for (String key : keys) {
+                        stringConnection.hGetAll(key);
+                    }
+                    return null;
+                }
+            });
+        } catch (Exception e){
+            log.error("query  OnlineShopRelWarehouse redis error");
+        }
+        assert objects != null;
+        return JsonHelper.stringToArray(JsonHelper.objectToString(objects), OnlineShopRelWarehouseCO.class);
     }
 }
