a98ecf56eed4fd1dc7412ea37a8d13cad28a3014
yipeng.zhu <yipeng.zhu@hand-china.com>
2022-07-07 11:46:01 +0800

    fix(master):修复删除运费模版详情lua错误问题

--- o2-metadata-management/src/main/resources/script/lua/freight/delete_freight_detail_cache.lua
+++ o2-metadata-management/src/main/resources/script/lua/freight/delete_freight_detail_cache.lua
@@ -7,19 +7,5 @@ local freightInforDeleteKey = KEYS[1];
 local freightDetailList = cjson.decode(ARGV[1]);
 
 for i,tmpId in pairs(freightDetailList) do
-    local oldJson = redis.call('hget', freightInforDeleteKey, tmpId);
-    if(oldJson)
-    then
-        local oldData = cjson.decode(oldJson);
-        local regionCode = (oldData["defaultFlag"] == '1') and oldData["regionCode"]  or 'DEFAULT';
-        local oldPriceKey = 'o2md:freight:'..oldData["tenantId"]..':'..oldData["templateCode"]..':'..regionCode;
-        redis.call('del', oldPriceKey);
-
-        if oldData["isDefault"] == 1 then
-            local defautPriceKey = 'o2md:freight:'..oldData["tenantId"]..':'..oldData["templateCode"]..':DEFAULT';
-            redis.call('del', defautPriceKey);
-        end
-    end
-
     redis.call('hdel', freightInforDeleteKey, tmpId);
 end
