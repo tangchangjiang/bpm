d43272e5ff14fbefd7d6f894c7b704c32f963dd5
wei.cai <wei.cai@hand-china.com>
2022-09-30 17:28:30 +0800

    feat(o2-metadata): 添加BPM + rule-engine
    添加BPM + rule-engine

---
+++ metadata/o2-metadata-management/src/main/java/org/o2/metadata/console/app/service/impl/PosTenantInitServiceImpl.java
@@ -0,0 +1,113 @@
+package org.o2.metadata.console.app.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.o2.metadata.console.app.service.PosService;
+import org.o2.metadata.console.app.service.PosTenantInitService;
+import org.o2.metadata.console.infra.constant.TenantInitConstants;
+import org.o2.metadata.console.infra.entity.Pos;
+import org.o2.metadata.console.infra.entity.PosAddress;
+import org.o2.metadata.console.infra.repository.PosAddressRepository;
+import org.o2.metadata.console.infra.repository.PosRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 服务点数据初始化
+ *
+ * @author tingting.wang@hand-china.com 2022-02-10
+ */
+@Service
+@Slf4j
+public class PosTenantInitServiceImpl implements PosTenantInitService {
+    private final PosService posService;
+    private final PosRepository posRepository;
+    private final PosAddressRepository addressRepository;
+
+    public PosTenantInitServiceImpl(PosService posService, PosRepository posRepository,
+                                    PosAddressRepository addressRepository) {
+        this.posService = posService;
+        this.posRepository = posRepository;
+        this.addressRepository = addressRepository;
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void tenantInitializeBusiness(Long sourceTenantId, Long targetTenantId) {
+        // 1. 查询源租户数据
+        log.info("Business: initializePos start, tenantId[{}]", targetTenantId);
+        Pos query = new Pos();
+        query.setTenantId(sourceTenantId);
+        query.setPosCodes(TenantInitConstants.InitPosBusiness.POS_CODE);
+        List<Pos> sourcePos = posService.selectByCondition(query);
+        if (CollectionUtils.isEmpty(sourcePos)) {
+            log.warn("Business data not exists in sourceTenantId[{}]", sourceTenantId);
+            return;
+        }
+        // 2. 查询目标租户数据
+        query.setTenantId(targetTenantId);
+        List<Pos> targetPos = posService.selectByCondition(query);
+        // 3. 操作数据
+        handleData(targetPos,sourcePos,targetTenantId);
+        log.info("Business: initializePos finish");
+    }
+
+    /**
+     * 处理网网店：更新已存在的，插入未存在的目标数据
+     * @param oldPos  已存在的数据
+     * @param initPos 初始化的数据
+     * @param targetTenantId 目标租户ID
+     */
+    private void handleData(List<Pos> oldPos, List<Pos> initPos, Long targetTenantId) {
+        // 3.1 更新目标数据
+        List<Pos> updateList = new ArrayList<>(4);
+        // 3.2 插入 目标数据
+        List<Pos> addList = new ArrayList<>(4);
+        for (Pos init : initPos) {
+            String posCode = init.getPosCode();
+            boolean addFlag = true;
+            if (oldPos.isEmpty()) {
+                addList.add(init);
+                continue;
+            }
+            for (Pos old : oldPos) {
+                String oldCode = old.getPosCode();
+                if (posCode.equals(oldCode)) {
+                    init.setTenantId(targetTenantId);
+                    init.setObjectVersionNumber(old.getObjectVersionNumber());
+                    init.setPosId(old.getPosId());
+                    updateList.add(init);
+                    addFlag = false;
+                    break;
+                }
+            }
+            if (addFlag) {
+                addList.add(init);
+            }
+        }
+
+        // 4. 更新目标库
+        posRepository.batchUpdateByPrimaryKey(updateList);
+        for (Pos pos : addList) {
+            pos.setPosId(null);
+            pos.setTenantId(targetTenantId);
+            // 关联服务地址
+            Long sourceAddressId = pos.getAddressId();
+            PosAddress query = new PosAddress();
+            query.setPosAddressId(sourceAddressId);
+            PosAddress address = addressRepository.selectByPrimaryKey(query);
+            if (null == address) {
+                address = new PosAddress();
+            }
+            address.setTenantId(targetTenantId);
+            address.setPosAddressId(null);
+            addressRepository.insert(address);
+            pos.setAddressId(address.getPosAddressId());
+            posRepository.insert(pos);
+
+        }
+    }
+}
