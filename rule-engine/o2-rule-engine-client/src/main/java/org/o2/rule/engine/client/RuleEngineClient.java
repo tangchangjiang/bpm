b20ccc1d5563c26ea9273a63e9e3f88e89fa4a9a
wei.cai <wei.cai@hand-china.com>
2022-10-11 14:09:41 +0800

    feat(o2-metadata): 初始化Rule-Engine-Client
    初始化Rule-Engine-Client

---
+++ rule-engine/o2-rule-engine-client/src/main/java/org/o2/rule/engine/client/RuleEngineClient.java
@@ -0,0 +1,15 @@
+package org.o2.rule.engine.client;
+
+/**
+ * 规则引擎客户端, 外部使用的唯一入口
+ *
+ * @author wei.cai@hand-china.com
+ * @date 2022/10/11
+ */
+public interface RuleEngineClient {
+
+
+
+
+
+}
---
+++ rule-engine/o2-rule-engine-client/src/main/java/org/o2/rule/engine/client/RuleEngineClientImpl.java
@@ -0,0 +1,13 @@
+package org.o2.rule.engine.client;
+
+/**
+ * 规则引擎客户端实现类, 外部使用的唯一入口
+ *
+ * @author wei.cai@hand-china.com
+ * @date 2022/10/11
+ */
+public class RuleEngineClientImpl implements RuleEngineClient {
+
+
+
+}
