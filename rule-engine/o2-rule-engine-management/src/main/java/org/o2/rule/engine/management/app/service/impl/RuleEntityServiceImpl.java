36e6ba9d371ab5c48e10b369d87300e7c89de8f0
yuncheng.ma <yuncheng.ma@hand-china.com>
2022-10-18 10:09:23 +0800

    feat(o2-metadata): 规则实体编码去掉编码规则

--- rule-engine/o2-rule-engine-management/src/main/java/org/o2/rule/engine/management/app/service/impl/RuleEntityServiceImpl.java
+++ rule-engine/o2-rule-engine-management/src/main/java/org/o2/rule/engine/management/app/service/impl/RuleEntityServiceImpl.java
@@ -1,23 +1,18 @@
 package org.o2.rule.engine.management.app.service.impl;
 
+import io.choerodon.mybatis.domain.AuditDomain;
 import org.hzero.core.base.BaseConstants;
 import org.hzero.mybatis.helper.UniqueHelper;
-import org.o2.code.builder.app.service.CodeBuildService;
-import org.o2.core.exception.O2CommonException;
-import org.o2.core.helper.UserHelper;
 import org.o2.rule.engine.management.app.service.RuleEntityService;
 import org.o2.rule.engine.management.domain.entity.RuleEntity;
 import org.o2.rule.engine.management.domain.repository.RuleEntityRepository;
-import org.o2.rule.engine.management.infra.constant.RuleEntityConstants;
 import org.o2.rule.engine.management.infra.converts.RuleEntityConverts;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import io.choerodon.mybatis.domain.AuditDomain;
-import org.springframework.util.ObjectUtils;
 
 /**
  * 规则实体应用服务默认实现
@@ -28,12 +23,9 @@ import org.springframework.util.ObjectUtils;
 public class RuleEntityServiceImpl implements RuleEntityService {
 
     private final RuleEntityRepository ruleEntityRepository;
-    private final CodeBuildService codeBuildService;
 
-    public RuleEntityServiceImpl(RuleEntityRepository ruleEntityRepository,
-                                 CodeBuildService codeBuildService) {
+    public RuleEntityServiceImpl(RuleEntityRepository ruleEntityRepository) {
         this.ruleEntityRepository = ruleEntityRepository;
-        this.codeBuildService = codeBuildService;
     }
 
     @Override
@@ -72,8 +64,6 @@ public class RuleEntityServiceImpl implements RuleEntityService {
         //保存规则实体
         UniqueHelper.valid(ruleEntity, RuleEntity.O2RE_RULE_ENTITY_U1);
         if (ruleEntity.getRuleEntityId() == null) {
-            String code = createRuleCode(ruleEntity.getTenantId());
-            ruleEntity.setRuleEntityCode(code);
             ruleEntityRepository.insertSelective(ruleEntity);
         } else {
             ruleEntityRepository.updateOptional(ruleEntity,
@@ -87,23 +77,5 @@ public class RuleEntityServiceImpl implements RuleEntityService {
         }
 
         return ruleEntity;
-    }
-
-    /**
-     * 创建编码
-     * @param tenantId 租户id
-     * @return code
-     */
-    private String createRuleCode(Long tenantId) {
-        try {
-            String code = codeBuildService.makePrimaryKey(tenantId, UserHelper.getUserId(),
-                    RuleEntityConstants.RULE_ENTITY_RULE_CODE);
-            if (ObjectUtils.isEmpty(code)) {
-                throw new O2CommonException(code, RuleEntityConstants.ErrorCode.RULE_CODE_ERROR);
-            }
-            return code;
-        } catch (Exception e) {
-            throw new O2CommonException(e, RuleEntityConstants.ErrorCode.RULE_CODE_ERROR);
-        }
     }
 }
