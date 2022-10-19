115ec0d84c2132e6124ce5d2aaf8e3ab462ecf1d
yuncheng.ma <yuncheng.ma@hand-china.com>
2022-10-19 17:46:54 +0800

    feat(o2-metadata): 是否启用查询修改

--- rule-engine/o2-rule-engine-management/src/main/java/org/o2/rule/engine/management/infra/repository/impl/RuleParamRepositoryImpl.java
+++ rule-engine/o2-rule-engine-management/src/main/java/org/o2/rule/engine/management/infra/repository/impl/RuleParamRepositoryImpl.java
@@ -3,7 +3,10 @@ package org.o2.rule.engine.management.infra.repository.impl;
 import org.hzero.mybatis.base.impl.BaseRepositoryImpl;
 import org.o2.rule.engine.management.domain.entity.RuleParam;
 import org.o2.rule.engine.management.domain.repository.RuleParamRepository;
+import org.o2.rule.engine.management.infra.mapper.O2reRuleParamMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * 规则参数 资源库实现
@@ -12,4 +15,14 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class RuleParamRepositoryImpl extends BaseRepositoryImpl<RuleParam> implements RuleParamRepository {
+    private final O2reRuleParamMapper o2reRuleParamMapper;
+
+    public RuleParamRepositoryImpl(O2reRuleParamMapper o2reRuleParamMapper) {
+        this.o2reRuleParamMapper = o2reRuleParamMapper;
+    }
+
+    @Override
+    public List<RuleParam> selectList(RuleParam ruleParam) {
+        return o2reRuleParamMapper.selectList(ruleParam);
+    }
 }
