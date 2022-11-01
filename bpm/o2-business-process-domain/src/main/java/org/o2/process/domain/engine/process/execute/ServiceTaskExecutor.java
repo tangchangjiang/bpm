cb792f8d02dfd35da1a392590fe06ad376effa37
汤长江 <changjiang.tang@hand-china.com>
2022-11-01 11:53:03 +0800

    feat(o2-process): 新增业务流程节点启用判断

--- bpm/o2-business-process-domain/src/main/java/org/o2/process/domain/engine/process/execute/ServiceTaskExecutor.java
+++ bpm/o2-business-process-domain/src/main/java/org/o2/process/domain/engine/process/execute/ServiceTaskExecutor.java
@@ -2,6 +2,7 @@ package org.o2.process.domain.engine.process.execute;
 
 import io.choerodon.core.convertor.ApplicationContextHelper;
 import io.choerodon.core.exception.CommonException;
+import org.hzero.core.base.BaseConstants;
 import org.o2.process.domain.engine.BusinessProcessExecParam;
 import org.o2.process.domain.engine.definition.Activity.ServiceTask;
 import org.o2.process.domain.engine.runtime.ProcessRuntimeContext;
@@ -19,22 +20,27 @@ public class ServiceTaskExecutor<T extends BusinessProcessExecParam> extends Bas
 
     @Override
     protected void doExecute(ProcessRuntimeContext<T> runtimeContext) {
+
         ServiceTask serviceTask = (ServiceTask) runtimeContext.getCurrentElement();
-        ServiceAction<T> action = ApplicationContextHelper.getContext().getBean(serviceTask.getBeanId(), ServiceAction.class);
-        // 设置当前参数
-        runtimeContext.getBusinessParam().setCurrentParam(serviceTask.getArgs());
 
-        action.beforeExecution(runtimeContext.getBusinessParam());
+        if(BaseConstants.Flag.YES.equals(serviceTask.getEnabledFlag())){
 
-        action.run(runtimeContext.getBusinessParam());
+            ServiceAction<T> action = ApplicationContextHelper.getContext().getBean(serviceTask.getBeanId(), ServiceAction.class);
+            // 设置当前参数
+            runtimeContext.getBusinessParam().setCurrentParam(serviceTask.getArgs());
 
-        action.afterExecution(runtimeContext.getBusinessParam());
+            action.beforeExecution(runtimeContext.getBusinessParam());
 
-        if(Boolean.FALSE.equals(runtimeContext.getBusinessParam().getNextFlag())){
-            if(runtimeContext.getBusinessParam().getException() != null){
-                throw new CommonException(runtimeContext.getBusinessParam().getException());
+            action.run(runtimeContext.getBusinessParam());
+
+            action.afterExecution(runtimeContext.getBusinessParam());
+
+            if(Boolean.FALSE.equals(runtimeContext.getBusinessParam().getNextFlag())){
+                if(runtimeContext.getBusinessParam().getException() != null){
+                    throw new CommonException(runtimeContext.getBusinessParam().getException());
+                }
+                throw new CommonException(ProcessEngineConstants.ErrorCode.BUSINESS_PROCESS_INTERRUPTED);
             }
-            throw new CommonException(ProcessEngineConstants.ErrorCode.BUSINESS_PROCESS_INTERRUPTED);
         }
 
         super.doExecute(runtimeContext);
