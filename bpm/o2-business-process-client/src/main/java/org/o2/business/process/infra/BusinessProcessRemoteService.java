b69f85b936d88a102bb434949f8a78461a32b9e1
汤长江 <changjiang.tang@hand-china.com>
2022-11-02 17:49:30 +0800

    feat(o2-bpm): 新增多值标记

--- bpm/o2-business-process-client/src/main/java/org/o2/business/process/infra/BusinessProcessRemoteService.java
+++ bpm/o2-business-process-client/src/main/java/org/o2/business/process/infra/BusinessProcessRemoteService.java
@@ -44,7 +44,7 @@ public interface BusinessProcessRemoteService {
                                              @PathVariable(value = "processCode") @ApiParam(value = "业务流程编码", required = true) String processCode);
 
 
-    @GetMapping("/{organizationId}/internal/business-process/{beanId}")
+    @GetMapping("/{organizationId}/internal/business-process/param_definition/{beanId}")
     ResponseEntity<String> getParamDefinition(@PathVariable(value = "organizationId") @ApiParam(value = "租户ID", required = true) Long organizationId,
                                                       @PathVariable(value = "beanId") @ApiParam(value = "业务流程编码", required = true) String beanId,
                                               @ApiParam(value = "参数编码", required = true) String paramCode);
