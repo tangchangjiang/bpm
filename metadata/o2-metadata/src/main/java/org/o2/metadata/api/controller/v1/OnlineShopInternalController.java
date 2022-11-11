4e94e84698bf4296cbc55bf9f46abe8cb1c5099c
lei.tong01 <lei.tong01@hand-china.com>
2022-11-11 14:08:03 +0800

    feat(o2-metadata): 修改C端网店名称查询
    修改C端网店名称查询

--- metadata/o2-metadata/src/main/java/org/o2/metadata/api/controller/v1/OnlineShopInternalController.java
+++ metadata/o2-metadata/src/main/java/org/o2/metadata/api/controller/v1/OnlineShopInternalController.java
@@ -40,7 +40,6 @@ public class OnlineShopInternalController {
     @Permission(permissionWithin = true, level = ResourceLevel.ORGANIZATION)
     @PostMapping("/online-shop/list")
     public ResponseEntity<List<OnlineShopCO>> queryOnlineShop(@RequestBody List<String> onlineShopCodes) {
-        UserHelper.validUserInfo(UserHelper.getUserInfo(), UserInfo.FIELD_TENANT_ID);
         return Results.success(onlineShopService.queryShopList(onlineShopCodes));
     }
 
