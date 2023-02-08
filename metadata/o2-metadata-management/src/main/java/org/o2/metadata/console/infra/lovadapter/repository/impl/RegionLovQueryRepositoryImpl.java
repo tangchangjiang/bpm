3d6f5d2e8e6c1a4c9ecac85c21a727be234e964d
yang chao <chao.yang05@hand-china.com>
2023-02-08 15:21:17 +0800

    feat(o2-metadata): 地区查询内部结构增加parentRegionCode查询字段

--- metadata/o2-metadata-management/src/main/java/org/o2/metadata/console/infra/lovadapter/repository/impl/RegionLovQueryRepositoryImpl.java
+++ metadata/o2-metadata-management/src/main/java/org/o2/metadata/console/infra/lovadapter/repository/impl/RegionLovQueryRepositoryImpl.java
@@ -179,7 +179,7 @@ public class RegionLovQueryRepositoryImpl implements RegionLovQueryRepository, A
         if (StringUtils.isNotEmpty(regionCode)) {
             regionList = regionList.stream().filter(region -> regionCode.equals(region.getRegionCode())).collect(Collectors.toList());
         }
-        // 父地区
+        // 父地区，是指查询地区的父地区编码是parentRegionCodes中的地区
         List<String> parentRegionCodes = queryLov.getParentRegionCodes();
         if (CollectionUtils.isNotEmpty(parentRegionCodes)) {
             regionList = regionList.stream().filter(region -> parentRegionCodes.contains(region.getRegionCode())).collect(Collectors.toList());
@@ -199,7 +199,7 @@ public class RegionLovQueryRepositoryImpl implements RegionLovQueryRepository, A
             regionList = regionList.stream().filter(region -> regionCodes.contains(region.getRegionCode())).collect(Collectors.toList());
 
         }
-        // 地区的上一级为
+        // 地区的上一级为，是指查询编码是parentRegionCode的父地区下的所有子地区
         String parentRegionCode = queryLov.getParentRegionCode();
         if (StringUtils.isNotEmpty(parentRegionCode)) {
             regionList = regionList.stream().filter(region -> parentRegionCode.equals(region.getParentRegionCode())).collect(Collectors.toList());
