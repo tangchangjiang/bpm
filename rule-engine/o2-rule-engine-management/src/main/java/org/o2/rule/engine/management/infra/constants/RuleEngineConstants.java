6c72fd79f20c8e6e8935742f213a24ce9efcf162
changjiang.tang <changjiang.tang@hand-china.com>
2023-02-09 15:09:19 +0800

    feat(o2-metadata-management): 规则引擎增加非空字符串判断逻辑

--- rule-engine/o2-rule-engine-management/src/main/java/org/o2/rule/engine/management/infra/constants/RuleEngineConstants.java
+++ rule-engine/o2-rule-engine-management/src/main/java/org/o2/rule/engine/management/infra/constants/RuleEngineConstants.java
@@ -55,6 +55,13 @@ public class RuleEngineConstants {
         public static final String NOT_IN = "notIn";
     }
 
+    public static class StringOperator {
+        /**
+         * 不为空
+         */
+        public static final String NOT_BLANK = "NotBlank";
+    }
+
     /**
      * 比较操作符
      *
@@ -99,6 +106,8 @@ public class RuleEngineConstants {
          * 集合类型
          */
         public static final String COLLECTION = "COLLECTION";
+
+        public static final String STRING = "STRING";
     }
 
     /**
