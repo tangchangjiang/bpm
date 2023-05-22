0c2f4e4a16ac12dd211a85ab7ac50dbc1541013b
yang chao <chao.yang05@hand-china.com>
2023-05-22 16:39:02 +0800

    feat(o2-metadata): 平台层维护规则引擎

---
+++ rule-engine/o2-rule-engine-management/src/main/java/org/o2/rule/engine/management/api/controller/v1/site/RuleParamSiteController.java
@@ -0,0 +1,9 @@
+package org.o2.rule.engine.management.api.controller.v1.site;
+
+/**
+ * description
+ *
+ * @author chao.yang05@hand-china.com 2023-05-22
+ */
+public class RuleParamSiteController {
+}
