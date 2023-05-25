c4b040a4e04e5aa57f4050c613b315e27d5fdcd5
miao.chen01 <miao.chen01@hand-china.com>
2023-05-25 15:44:21 +0800

    feat(o2-metadata):添加多语言

--- metadata/o2-metadata/src/main/java/org/o2/metadata/infra/constants/CarrierConstants.java
+++ metadata/o2-metadata/src/main/java/org/o2/metadata/infra/constants/CarrierConstants.java
@@ -1,7 +1,6 @@
 package org.o2.metadata.infra.constants;
 
 /**
- *
  * 承运商
  *
  * @author yipeng.zhu@hand-china.com 2021-08-05
@@ -16,6 +15,10 @@ public interface CarrierConstants {
          * redis key(hash): o2md:carrier:{tenantId}
          */
         String CARRIER_KEY = "o2md:carrier:{%s}:all";
+        /**
+         * redis key(hash): o2md:carrier:{tenantId}:{carrierCode}
+         */
+        String CARRIER_MULTI_KEY = "o2md:carrier:{%s}:%s";
 
         /**
          * GetFreightDefaultKey
@@ -25,6 +28,17 @@ public interface CarrierConstants {
          */
         static String getCarrierKey(Long tenantId) {
             return String.format(CARRIER_KEY, tenantId);
+        }
+
+        /**
+         * 获取多语言key
+         *
+         * @param tenantId    租户id
+         * @param carrierCode code
+         * @return key
+         */
+        static String getCarrierMultiKey(Long tenantId, String carrierCode) {
+            return String.format(CARRIER_MULTI_KEY, tenantId, carrierCode);
         }
     }
 }
